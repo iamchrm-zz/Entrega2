b2272d37e4a62e95e5575de72eb1e3c5d8182cb9
iamchrm <53318723+iamchrm@users.noreply.github.com>
2020-11-17 01:11:39 -0300

    update

--- entrega2/proComida/core/views.py
+++ entrega2/proComida/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request,'core/home.html')
+
+def productos(request):
+    return render(request,'core/productos.html')
--- entrega2/proComida/proComida/urls.py
+++ entrega2/proComida/proComida/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('productos/', views.productos, name='productos'),
     path('admin/', admin.site.urls),
+    
 ]
