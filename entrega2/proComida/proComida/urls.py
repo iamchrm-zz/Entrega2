b74ecdc8cf16bb18fb6ecb763a0bdcfe93fa8c9a
iamchrm <53318723+iamchrm@users.noreply.github.com>
2020-11-30 16:10:45 -0300

    a

--- entrega2/proComida/proComida/urls.py
+++ entrega2/proComida/proComida/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from core import views as core_views
 from productos import views as productos_views
+
 from django.conf import settings
 urlpatterns = [
     path('', core_views.home, name='home'),
     path('productos/', productos_views.productos, name='productos'),
+
     path('admin/', admin.site.urls),
     
 ]
--- entrega2/proComida/productos/admin.py
+++ entrega2/proComida/productos/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, ProductoP
 
 # Register your models here.
 class ProductoAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
+class ProductoPAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')    
 
 admin.site.register(Producto, ProductoAdmin)
+admin.site.register(ProductoP, ProductoPAdmin)
